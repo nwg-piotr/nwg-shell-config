82dfc148fd802a13426776399eef55f8f3d8d1b5
nwg-piotr <nwg.piotr@gmail.com>
2021-12-28 14:53:54 +0100

    fix uninstall (another try)

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 path=$(python -c 'import site; print(site.getsitepackages()[0])')
-sudo rm -r $path'/nwg_shell_config*'
-sudo rm /usr/bin/nwg-shell-config
-sudo rm /usr/share/applications/nwg-shell-config.desktop
+rm -r $path'/nwg_shell_config*'
+rm /usr/bin/nwg-shell-config
+rm /usr/share/applications/nwg-shell-config.desktop
