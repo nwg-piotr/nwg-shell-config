a0cd742f9bd499f4ffc460c19ddea98589302b61
nwg-piotr <nwg.piotr@gmail.com>
2021-12-28 11:51:03 +0100

    test2

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 path=$(python -c 'import site; print(site.getsitepackages()[0])')
-rm -r $path'/nwg_shell_config*/'
+sh -c 'rm -r $path/nwg_shell_config*'
 rm /usr/bin/nwg-shell-config
 rm /usr/share/applications/nwg-shell-config.desktop
