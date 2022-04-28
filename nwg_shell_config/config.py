df18bec48636f652a478ac1919e525a01ed7e4a8
nwg-piotr <nwg.piotr@gmail.com>
2022-04-28 16:05:20 +0200

    screen tab

--- nwg_shell_config/config.py
+++ nwg_shell_config/config.py
@@ -7,6 +7,7 @@ import argparse
 import gi
 
 gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk, GLib
 
 from nwg_shell_config.tools import *
--- nwg_shell_config/ui_components.py
+++ nwg_shell_config/ui_components.py
@@ -6,9 +6,9 @@ from gi.repository import Gtk
 
 def side_menu():
     list_box = Gtk.ListBox()
-    list_box.set_property("margin", 6)
+    list_box.set_property("margin-top", 6)
 
-    row = SideMenuRow("Screen configuration")
+    row = SideMenuRow("Screen settings")
     list_box.add(row)
 
     row = SideMenuRow("Keyboard")
@@ -57,16 +57,14 @@ class SideMenuRow(Gtk.ListBoxRow):
 
 def screen_tab():
     frame = Gtk.Frame()
-    # frame.set_size_request(640, 480)
-    frame.set_label(" Screen configuration ")
+    frame.set_label("  Screen settings  ")
     frame.set_label_align(0.5, 0.5)
-    frame.set_property("margin", 12)
     frame.set_property("hexpand", True)
     grid = Gtk.Grid()
     frame.add(grid)
-    grid.set_property("margin", 12)
-    grid.set_column_spacing(12)
-    grid.set_column_homogeneous(True)
+    grid.set_property("margin", 6)
+    grid.set_column_spacing(6)
+    grid.set_row_spacing(6)
 
     btn = Gtk.Button()
     btn.set_property("name", "app-btn")
@@ -92,8 +90,81 @@ def screen_tab():
     btn.set_image_position(Gtk.PositionType.TOP)
     img = Gtk.Image.new_from_icon_name("nwg-look", Gtk.IconSize.DIALOG)
     btn.set_image(img)
-    btn.set_label("Look and feel")
+    btn.set_label("Look & feel")
     grid.attach(btn, 2, 0, 1, 1)
+
+    lbl = Gtk.Label()
+    lbl.set_markup("<b>Autotiling</b>")
+    lbl.set_property("margin-top", 6)
+    lbl.set_property("margin-bottom", 6)
+    lbl.set_property("halign", Gtk.Align.START)
+    grid.attach(lbl, 0, 1, 1, 1)
+
+    cb_autotiling_on = Gtk.CheckButton.new_with_label("on")
+    grid.attach(cb_autotiling_on, 1, 1, 1, 1)
+
+    lbl = Gtk.Label.new("Workspaces:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 0, 2, 1, 1)
+
+    entry = Gtk.Entry()
+    entry.set_placeholder_text("1 2 3 4 5 6 7 8")
+    grid.attach(entry, 1, 2, 1, 1)
+
+    lbl = Gtk.Label()
+    lbl.set_markup("<b>Night light</b>")
+    lbl.set_property("margin-top", 6)
+    lbl.set_property("margin-bottom", 6)
+    lbl.set_property("halign", Gtk.Align.START)
+    grid.attach(lbl, 0, 3, 1, 1)
+
+    cb_night_light_on = Gtk.CheckButton.new_with_label("on")
+    grid.attach(cb_night_light_on, 1, 3, 1, 1)
+
+    lbl = Gtk.Label.new("Latitude:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 0, 4, 1, 1)
+
+    sb_lat = Gtk.SpinButton.new_with_range(-90.0, 90.0, 0.1)
+    sb_lat.set_digits(4)
+    grid.attach(sb_lat, 1, 4, 1, 1)
+
+    lbl = Gtk.Label.new("Temp low:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 2, 4, 1, 1)
+
+    sb_temp_low = Gtk.SpinButton.new_with_range(1000, 10000, 100)
+    grid.attach(sb_temp_low, 3, 4, 1, 1)
+
+    lbl = Gtk.Label.new("Longitude:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 0, 5, 1, 1)
+
+    sb_lon = Gtk.SpinButton.new_with_range(-180, 180, 0.1)
+    sb_lon.set_digits(4)
+    grid.attach(sb_lon, 1, 5, 1, 1)
+
+    lbl = Gtk.Label.new("Temp high:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 2, 5, 1, 1)
+
+    sb_temp_high = Gtk.SpinButton.new_with_range(1000, 10000, 100)
+    grid.attach(sb_temp_high, 3, 5, 1, 1)
+
+    lbl = Gtk.Label.new("Gamma:")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 0, 6, 1, 1)
+
+    sb_gamma = Gtk.SpinButton.new_with_range(0.1, 10.0, 0.1)
+    grid.attach(sb_gamma, 1, 6, 1, 1)
+
+    lbl = Gtk.Label()
+    lbl.set_markup("<b>Help widget</b>")
+    lbl.set_property("halign", Gtk.Align.END)
+    grid.attach(lbl, 0, 7, 1, 1)
+
+    cb_help_on = Gtk.CheckButton.new_with_label("on")
+    grid.attach(cb_help_on, 1, 7, 1, 1)
 
     frame.show_all()
 
