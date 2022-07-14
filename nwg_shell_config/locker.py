49cfdea756ff2a9ae489fd846eaba5ec7deaab71
piotr <nwg.piotr@gmail.com>
2022-07-15 00:36:56 +0200

    kill nwg-lock

--- nwg_shell_config/locker.py
+++ nwg_shell_config/locker.py
@@ -232,7 +232,7 @@ def set_remote_wallpaper():
             if settings["lockscreen-locker"] == "swaylock":
                 subprocess.Popen('swaylock -i {} && kill -n 15 {}'.format(wallpaper, pid), shell=True)
             elif settings["lockscreen-locker"] == "gtklock":
-                subprocess.Popen('pkill -f gtklock && gtklock -i -b {} && kill -n 15 {}'.format(wallpaper, pid), shell=True)
+                subprocess.Popen('gtklock -i -b {} && kill -n 15 {}'.format(wallpaper, pid), shell=True)
 
             if pctl:
                 Gdk.threads_add_timeout_seconds(GLib.PRIORITY_LOW, 1, pctl.refresh)
