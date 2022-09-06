fca22f30d4fe2216cdfda2c916d8f5fe8d851e30
piotr <nwg.piotr@gmail.com>
2022-09-06 02:57:50 +0200

    remove unused var

--- nwg_shell_config/updater.py
+++ nwg_shell_config/updater.py
@@ -43,9 +43,6 @@ lock_file = os.path.join(temp_dir(), "nwg-shell-updater.lock")
 config_home = os.getenv('XDG_CONFIG_HOME') if os.getenv('XDG_CONFIG_HOME') else os.path.join(os.getenv("HOME"),
                                                                                              ".config")
 
-data_home = os.getenv('XDG_DATA_HOME') if os.getenv('XDG_DATA_HOME') else os.path.join(os.getenv("HOME"),
-                                                                                       ".local/share")
-
 
 def terminate(*args):
     if os.path.isfile(lock_file):
