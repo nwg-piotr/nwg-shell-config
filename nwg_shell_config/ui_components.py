71d8f3335e6a5aee10010014d123882c20d4af78
piotr <nwg.piotr@gmail.com>
2022-11-05 02:55:24 +0100

    add depth limit description

--- nwg_shell_config/ui_components.py
+++ nwg_shell_config/ui_components.py
@@ -552,6 +552,15 @@ def autotiling_tab(settings, outputs, voc):
         sb.set_tooltip_text(voc["autotiling-depth-limit-tooltip"])
         grid.attach(sb, 1, 3 + i, 1, 1)
 
+    lbl = Gtk.Label()
+    lbl.set_line_wrap(True)
+    lbl.set_markup("Depth limit determines how deep will autotiling work. "
+                   "The '2' value allows to mimic the master/stack layout on horizontal displays. "
+                   "On vertical displays you may like the '3' value. Use '0' for no limit. "
+                   "Beyond the limit, tabbed and stacking layouts will work back again. "
+                   "The '1' value is not recommended, as it will spoil autotiling.")
+    grid.attach(lbl, 2, 3, 1, 3)
+
     frame.show_all()
 
     return frame
