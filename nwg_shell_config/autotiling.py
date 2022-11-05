fdba13260b713ca08141e14d34de13834e67767a
piotr <nwg.piotr@gmail.com>
2022-11-05 02:35:31 +0100

    undo changes made for debugging

--- nwg_shell_config/autotiling.py
+++ nwg_shell_config/autotiling.py
@@ -33,12 +33,13 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from nwg_shell_config.tools import temp_dir, get_data_dir, load_json, check_key, notify
+from nwg_shell_config.tools import temp_dir, get_data_dir, load_json, check_key
 
 # The `~/.local/share/nwg-shell-config/settings` json file should contain the "autotiling-output-limits" key,
 # e.g. like this: "autotiling-output-limits": {"DP-1": 3, "HDMI-A-1": 2, "eDP-1": 2}
-settings = {}
-
+settings = load_json(os.path.join(get_data_dir(), "settings"))
+# Set None if it does not. It's also the value to turn limits off.
+check_key(settings, "autotiling-output-limits", {})
 
 
 def save_string(string, file):
@@ -137,14 +138,6 @@ def signal_handler(sig, frame):
 
 
 def main():
-    # The `~/.local/share/nwg-shell-config/settings` json file should contain the "autotiling-output-limits" key,
-    # e.g. like this: "autotiling-output-limits": {"DP-1": 3, "HDMI-A-1": 2, "eDP-1": 2}
-    global settings
-    settings = load_json(os.path.join(get_data_dir(), "settings"))
-    # Set None if it does not. It's also the value to turn limits off.
-    check_key(settings, "autotiling-output-limits", {})
-    notify("autotiling", settings["autotiling-output-limits"])
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-d",
                         "--debug",
