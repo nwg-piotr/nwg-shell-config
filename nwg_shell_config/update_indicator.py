a22e8666a97fca75fcfa3842c5864e4693278c5f
piotr <nwg.piotr@gmail.com>
2023-01-20 14:09:38 +0100

    localize "Press Enter to exit..."

--- nwg_shell_config/update_indicator.py
+++ nwg_shell_config/update_indicator.py
@@ -195,7 +195,7 @@ class Indicator(object):
         # You just need to provide your own `nwg-system-update` script somewhere on $PATH.
         # IMPORTANT: the presence of this script will also determine if to display corresponding
         # options in the config utility.
-        subprocess.call('exec foot nwg-system-update', shell=True)
+        subprocess.call("exec foot nwg-system-update '{}'".format(voc["press-enter-to-exit"]), shell=True)
 
         self.check_updates()
 
