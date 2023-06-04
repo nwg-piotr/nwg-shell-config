9e808c917a80beebb2d223dbcf03af9bf16047eb
piotr <nwg.piotr@gmail.com>
2023-06-04 13:10:01 +0200

    simplify main.py

--- nwg_shell_config/main.py
+++ nwg_shell_config/main.py
@@ -21,20 +21,15 @@ arguments = " ".join(sys.argv[1:])
 def main():
     if os.getenv("SWAYSOCK"):
         cmd = "nwg-shell-config-sway {}".format(arguments)
-        try:
-            subprocess.Popen(cmd.split())
-        except FileNotFoundError as e:
-            eprint(e)
-            sys.exit(1)
     elif os.getenv("HYPRLAND_INSTANCE_SIGNATURE"):
         cmd = "nwg-shell-config-hyprland {}".format(arguments)
-        try:
-            subprocess.Popen(cmd.split())
-        except FileNotFoundError as e:
-            eprint(e)
-            sys.exit(1)
     else:
         eprint("This program needs either sway or Hyprland environment")
+        sys.exit(1)
+    try:
+        subprocess.Popen(cmd.split())
+    except Exception as e:
+        eprint(e)
         sys.exit(1)
 
 
