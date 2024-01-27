cb8feb9711afcd60684a5bb596b1dbfbceb8f723
piotr <nwg.piotr@gmail.com>
2024-01-27 02:29:58 +0100

    bump to 0.5.32

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-shell-config',
-    version='0.5.31',
+    version='0.5.32',
     description='nwg-shell configuration utility',
     packages=find_packages(),
     include_package_data=True,
