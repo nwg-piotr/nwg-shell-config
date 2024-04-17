d4e2fd26e84018fff59c810baac1660042b64e2f
piotr <nwg.piotr@gmail.com>
2024-04-17 03:10:50 +0200

    add dialog.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        "": ["glade/*", "autotranslate/*", "langs/*", "shell/*", "updates/*"]
+        "": ["glade/*", "autotranslate/*", "langs/*", "dialog/*", "shell/*", "updates/*"]
     },
     url='https://github.com/nwg-piotr/nwg-shell-config',
     license='MIT',
