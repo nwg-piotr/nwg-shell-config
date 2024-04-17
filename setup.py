1750028c5df9ee4df38db5c3469f6dfd0b5a45b0
piotr <nwg.piotr@gmail.com>
2024-04-17 12:31:30 +0200

    add entry point; bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-shell-config',
-    version='0.5.35',
+    version='0.5.36',
     description='nwg-shell configuration utility',
     packages=find_packages(),
     include_package_data=True,
@@ -34,7 +34,8 @@ setup(
             'nwg-shell-updater = nwg_shell_config.updater:main',
             'nwg-shell-translate = nwg_shell_config.translate:main',
             'nwg-update-indicator = nwg_shell_config.update_indicator:main',
-            'nwg-screenshot-applet = nwg_shell_config.screenshot_applet:main'
+            'nwg-screenshot-applet = nwg_shell_config.screenshot_applet:main',
+            'nwg-dialog = nwg_shell_config.dialog:main'
         ]
     }
 )
