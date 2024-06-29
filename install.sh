5787099c81e5161623f0faaff0c9247404a8bf47
piotr <nwg.piotr@gmail.com>
2024-06-29 22:43:47 +0200

    remove all launchers

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ done
 # Remove launcher scripts
 filenames=("/usr/bin/nwg-autotranslate" "/usr/bin/nwg-shell-config" "/usr/bin/nwg-shell-config-sway" "/usr/bin/nwg-shell-config-hyprland" "/usr/bin/nwg-lock" "/usr/bin/nwg-shell-help" "/usr/bin/nwg-autotiling" "/usr/bin/nwg-shell-updater" "/usr/bin/nwg-shell-translate" "/usr/bin/nwg-update-indicator" "/usr/bin/nwg-screenshot-applet" "/usr/bin/nwg-dialog")
 for filename in "${filenames[@]}"; do
-  if [ -f filename ]; then
+  if [ -f "$filename" ]; then
     echo "Removing $filename"
     rm filename
   fi
