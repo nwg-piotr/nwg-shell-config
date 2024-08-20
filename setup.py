1d306ec58ad42e63c8c89e7fdca9b44fb858a62c
piotr <nwg.piotr@gmail.com>
2024-08-20 20:51:30 +0200

    remove obsolete Updates btn

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
             'nwg-lock = nwg_shell_config.locker:main',
             'nwg-shell-help = nwg_shell_config.help:main',
             'nwg-autotiling = nwg_shell_config.autotiling:main',
-            'nwg-shell-updater = nwg_shell_config.updater:main',
             'nwg-shell-translate = nwg_shell_config.translate:main',
             'nwg-update-indicator = nwg_shell_config.update_indicator:main',
             'nwg-screenshot-applet = nwg_shell_config.screenshot_applet:main',
