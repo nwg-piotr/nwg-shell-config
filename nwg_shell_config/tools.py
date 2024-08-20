eb3c51ac404cb52b6b7ae3e9f2950c1c3a968029
piotr <nwg.piotr@gmail.com>
2024-08-20 21:38:39 +0200

    fix obtaining lat, lon

--- nwg_shell_config/tools.py
+++ nwg_shell_config/tools.py
@@ -228,20 +228,20 @@ def h_list_devices_by_type(device_type):
 def get_lat_lon():
     eprint("Checking location for night light settings")
     # Placeholder in case something goes wrong
-    tz, lat, lon = "Europe/Warsaw", 52.2322, 20.9841
+    tz, lat, lon = "Warsaw", 52.2322, 20.9841
 
     lines = get_command_output("timedatectl show")
     if lines:
         for line in lines:
             if line.startswith("Timezone="):
-                tz = line.split("=")[1]
+                tz = line.split("=")[1].split("/")[-1]
         try:
             geolocator = Nominatim(user_agent="my_request")
             location = geolocator.geocode(tz)
             lat = round(location.latitude, 5)
             lon = round(location.longitude, 5)
         except:
-            print("Geocoder unavailable. Do you have internet connection?", file=sys.stderr)
+            print(f"Couldn't find coordinates for {tz}, setting Warsaw, 52.2322, 20.9841", file=sys.stderr)
 
     return tz, lat, lon
 
