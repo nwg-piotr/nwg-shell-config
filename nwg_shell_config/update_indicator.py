f0f42eda047bf8cc91766f4b7883a367d4bd0513
piotr <nwg.piotr@gmail.com>
2024-08-20 21:08:50 +0200

    support just pacman, w/o AUR helpers

--- nwg_shell_config/update_indicator.py
+++ nwg_shell_config/update_indicator.py
@@ -189,8 +189,16 @@ class Indicator(object):
         # Below we could add update check commands for other distros
         global nwg_system_update_arg
         if self.distro == "arch":
-            nwg_system_update_arg = "-baph"
-            if is_command("yay"):
+            nwg_system_update_arg = "-pacman"
+
+            if is_command("baph"):
+                eprint("Using baph")
+                output = subprocess.check_output("baph -c".split()).decode('utf-8').strip()
+                if output and output != "0 0":
+                    u = output.split()
+                    update_details = f"pacman: {u[1]}, AUR: {u[0]}"
+
+            elif is_command("yay"):
                 nwg_system_update_arg = "-yay"
                 eprint("Using yay")
                 pacman, aur = 0, 0
@@ -208,12 +216,17 @@ class Indicator(object):
                 if pacman or aur:
                     update_details = f"pacman: {pacman}, AUR: {aur}"
 
-            elif is_command("baph"):
-                eprint("Using baph")
-                output = subprocess.check_output("baph -c".split()).decode('utf-8').strip()
-                if output and output != "0 0":
-                    u = output.split()
-                    update_details = f"pacman: {u[1]}, AUR: {u[0]}"
+            else:
+                eprint("Using checkupdates")
+                pacman = 0
+                try:
+                    output = subprocess.check_output("checkupdates".split()).decode('utf-8').splitlines()
+                    pacman = len(output)
+                except subprocess.CalledProcessError:
+                    pass
+
+                if pacman:
+                    update_details = f"pacman: {pacman}"
 
             if update_details:
                 eprint(update_details)
