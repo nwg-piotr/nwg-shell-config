92418879356dd4dd00c611d56f47285d3fd6e7c0
piotr <nwg.piotr@gmail.com>
2024-12-04 01:54:43 +0100

    disallow multiple instances

--- nwg_shell_config/hud.py
+++ nwg_shell_config/hud.py
@@ -71,7 +71,7 @@ def update_image(image, icon_name, icon_size):
 def main():
     for proc in process_iter():
         if "nwg-hud" in proc.name() and proc.pid != os.getpid():
-            eprint("[nwg-hud]\nRunning instance found, exiting")
+            eprint("nwg-hud: running instance found, terminating")
             sys.exit(1)
 
     # initiate config files if not found
